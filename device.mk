fdf2cee27496ba1be21dfc231218f84cdddc7f4e
guaiyihu <guaiyihu@foxmail.com>
2018-05-29 00:21:42 +0800

    whyred: Add XiaomiParts
    Change-Id: I4937914b9569d08e2b556ee233cd01c105d731f9
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -460,3 +460,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
