110875d77f6315ebe245c999063696991d083b2e
merothh <merothh@gmail.com>
2018-06-28 23:59:50 +0530

    whyred: nuke some lineage stuff

--- device.mk
+++ device.mk
@@ -28,7 +28,6 @@ $(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-	$(LOCAL_PATH)/overlay-lineage
 	
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
@@ -296,10 +295,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 	
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm	
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
