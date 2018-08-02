5f743adc815611d3cb5517f2deef56ccacb290a4
taran107 <hora.cooltaran@gmail.com>
2018-08-02 18:56:09 +0000

    whyred: nuke some lineage stuff

--- device.mk
+++ device.mk
@@ -17,8 +17,7 @@
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
