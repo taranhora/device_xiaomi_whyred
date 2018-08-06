aebd43380608c2d60fed2dde3ffe532a9055bab5
taran107 <hora.cooltaran@gmail.com>
2018-08-06 06:07:00 +0000

    whyred:include MiuiCamera

--- device.mk
+++ device.mk
@@ -146,8 +146,7 @@ PRODUCT_PACKAGES += \
     libbt-vendor
 	
 # Camera
-PRODUCT_PACKAGES += \
-	Snap
+$(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
